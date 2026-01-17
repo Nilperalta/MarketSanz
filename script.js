283003e5426428617976ff61beb27493e1baecda
Nilperalta <nildalisset.ll@gmail.com>
2026-01-16 19:44:24 -0500

    Botones enlazados a whatssap

--- script.js
+++ script.js
@@ -33,6 +33,27 @@ function prev(){
 
 
 
+const phoneNumber = "51926588816"; // 👈 número del negocio
+
+document.querySelectorAll(".btn-whatsapp").forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        const box = btn.closest(".box");
+
+        const name = box.querySelector(".product-name").innerText;
+        const price = box.querySelector(".product-price").innerText;
+        const img = box.querySelector(".product-img").src;
+
+        const message = `Hola, quiero pedir este producto:%0A%0A` +
+                        `*${name}*%0A` +
+                        `Precio: ${price}%0A` +
+                        `Imagen: ${img}`;
+
+        const url = `https://wa.me/${phoneNumber}?text=${message}`;
+
+        window.open(url, "_blank");
+    });
+});
 
 
 
